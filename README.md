6ca9f6a15b06d231194c593801aff21ec9dd9ec2
mayara <mayarabcoelho@gmail.com>
2024-07-29 11:46:44 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Página Spotify
+
+Projeto de reprodução da interface do Spotify desenvolvido com HTML, CSS e Javascript no evento da empresa Alura. Consiste na recriação de menus e botões com a área principal destinada aos cards dos artistas, onde há o uso do scroll para visualizar todos os cards.
+
+![Tela principal](./src/assets/tela.png)
---
+++ script.js
@@ -0,0 +1,23 @@
+const greetingElement = document.getElementById("greeting");
+
+const currentHour = new Date().getHours();
+
+const greetingMessage =
+    currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    :currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    :"Boa noite";
+
+greetingElement.textContent = greetingMessage;
+
+const container = document.querySelector(".offer__list-item");
+
+const observer = new ResizeObserver( () => {
+    const containerWidth = container.offsetWidth;
+    const numColumns = Math.floor(containerWidth / 200); 
+    
+    container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
+});
+
+observer.observe(container);
